791588cebb65408a5daf9b4047ce5a91b39fc347
Xianjin YE <advancedxy@gmail.com>
2016-01-14 22:52:59 +0800

    Initial commit. Process to if-and-else.go

---
+++ named-results.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Naked return should only used in small functions
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
---
+++ type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42.14
+	fmt.Printf("v is the type of %T\n", v)
+}
